0182bee5921bf510c8208e391a3439fb3bbebeb0
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-21 11:45:33 +0200

    ++++ scraper.pl
    Signed-off-by: Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>

--- scraper.pl
+++ scraper.pl
@@ -5,16 +5,16 @@ Log::Log4perl->easy_init($ERROR);
 use WWW::Mechanize::Chrome;
 
 my $mech = WWW::Mechanize::Chrome->new(
-  autodie => 0,               # Treat HTTP errors as non-fatal
-  headless => 0,              # Disable Chrome built-in headless mode
-  host => '127.0.0.1',        # Host on which DevTools listens for commands
-  port => 9222,               # Port on which DevTools listens for commands
-  cleanup_signal => 'TERM',   # The signal that is sent to the browser to stop it
-  start_url => 'about:blank', # Immediately navigate to a given address
-  autoclose => 0,             # Do not close browser at the end of execution
-  separate_session => 1,      # Create a new session without cookies
-  startup_timeout => 10,      # Time waiting for a browser to launch
-  mute_audio => 0,            # Disable audio channel
+   autodie => 0,               # Treat HTTP errors as non-fatal
+   headless => 0,              # Disable Chrome built-in headless mode
+   host => '127.0.0.1',        # Host on which DevTools listens for commands
+   port => 9222,               # Port on which DevTools listens for commands
+   cleanup_signal => 'TERM',   # The signal that is sent to the browser to stop it
+   start_url => 'about:blank', # Immediately navigate to a given address
+   autoclose => 0,             # Do not close browser at the end of execution
+   separate_session => 1,      # Create a new session without cookies
+   startup_timeout => 10,      # Time waiting for a browser to launch
+   mute_audio => 0,            # Disable audio channel
 );
 
 print "Web-scraper execution ...";
@@ -23,7 +23,7 @@ $mech->get('https://www.cloudflare.com/');
 $mech->sleep( 6 );
 
 my $page_pdf = $mech->content_as_pdf(
-    filename => 'cloudflare.pdf',
+   filename => 'cloudflare.pdf',
 );
 
 print " OK\n";
