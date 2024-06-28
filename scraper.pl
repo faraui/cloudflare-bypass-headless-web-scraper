4bcc14047b6c1789e33e511a4298ae43342bcd51
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-29 00:19:08 +0200

    ++++ scraper.pl
    Signed-off-by: Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>

--- scraper.pl
+++ scraper.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use open qw( :std :encoding(UTF-8) );
+use open qw(:std :encoding(UTF-8));
 use Log::Log4perl qw(:easy);
 Log::Log4perl->easy_init($ERROR);
 use WWW::Mechanize::Chrome;
@@ -8,7 +8,7 @@ use WWW::Mechanize::Chrome;
 my $mech = WWW::Mechanize::Chrome->new(
    host => '127.0.0.1',        # Host on which DevTools listens for commands
    port => 9222,               # Port on which DevTools listens for commands
-   start_url => 'about:blank', # Immediately navigate to a given address
+   start_url => "about:blank", # Immediately navigate to a given address
    autoclose => 0,             # Do not close browser at the end of execution
    separate_session => 1,      # Create a new session without cookies
    startup_timeout => 10,      # Time waiting for a browser to launch
@@ -18,13 +18,13 @@ my $mech = WWW::Mechanize::Chrome->new(
    cleanup_signal => 'TERM',   # The signal that is sent to the browser to stop it
 );
 
-print STDERR "Web-scraper execution ...";
+print STDERR 'Web-scraper execution ...';
 
-$mech->get('https://www.cloudflare.com/');
-$mech->sleep( 8 );
+$mech->get("https://www.cloudflare.com/");
+$mech->sleep(8);
 
 my $page_pdf = $mech->content_as_pdf(
    filename => 'cloudflare.pdf',
 );
 
-print STDERR " OK\n";
+print STDERR ' OK\n';
