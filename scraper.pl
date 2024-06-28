09772b3071924e76b2617a82a93102cfbba91c28
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-28 23:45:49 +0200

    +++++ scraper.pl
    Signed-off-by: Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>

--- scraper.pl
+++ scraper.pl
@@ -6,16 +6,16 @@ Log::Log4perl->easy_init($ERROR);
 use WWW::Mechanize::Chrome;
 
 my $mech = WWW::Mechanize::Chrome->new(
-   autodie => 0,               # Treat HTTP errors as non-fatal
-   headless => 0,              # Disable Chrome built-in headless mode
    host => '127.0.0.1',        # Host on which DevTools listens for commands
    port => 9222,               # Port on which DevTools listens for commands
-   cleanup_signal => 'TERM',   # The signal that is sent to the browser to stop it
    start_url => 'about:blank', # Immediately navigate to a given address
    autoclose => 0,             # Do not close browser at the end of execution
    separate_session => 1,      # Create a new session without cookies
    startup_timeout => 10,      # Time waiting for a browser to launch
-   mute_audio => 0,            # Disable audio channel
+   autodie => 0,               # Treat HTTP errors as non-fatal
+   headless => 0,              # Disable browser built-in headless mode
+   mute_audio => 1,            # Disable audio channel
+   cleanup_signal => 'TERM',   # The signal that is sent to the browser to stop it
 );
 
 print STDERR "Web-scraper execution ...";
